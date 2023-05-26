071d0a5ec5046534ae3a0c8b39deb5ad8bc3d6a9
kennywindis <kennyphilip100@gmail.com>
2023-05-25 18:01:39 -0700

    pd

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Encrypt passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ a hash_password function that expects one string argument name password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ expects 2 arguments and returns a boolean. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
