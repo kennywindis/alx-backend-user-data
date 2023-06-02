5e0bea30ffb08584b7b1f087479ebe0c0645a977
kennywindis <kennyphilip100@gmail.com>
2023-06-02 15:24:20 -0700

    sa

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-"""  UserSession class. """
-
+""" UserSession module
+"""
 from models.base import Base
 
 
 class UserSession(Base):
-    """ Extend behaviors of Base class for session authentication using DB.
+    """ UserSession class
     """
 
-    def __init__(self, *args: list, **kwargs: dict):
-        ''' Initialize class instance. '''
-        super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
