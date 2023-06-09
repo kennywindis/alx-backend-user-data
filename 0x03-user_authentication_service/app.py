cd0a00a624f5dfc28ccf6e5ca89985db71fe2324
kennywindis <kennyphilip100@gmail.com>
2023-06-08 23:54:03 -0700

    ua

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-""" App Module """
+""" Basic Flask app, Register user, Log in, Log out, User profile,
+    Get reset passwords token, Update password end-point """
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome():
     """ Basic Flask app, return a JSON """
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
@@ -65,6 +68,7 @@ def get_reset_password_token():
         return jsonify({"email": email, "reset_token": token}), 200
     except ValueError:
         abort(403)
+
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
