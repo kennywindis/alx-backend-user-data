6f7d590c0391240832db8dc9d725b4226a09e32c
kennywindis <kennyphilip100@gmail.com>
2023-06-28 10:13:49 -0700

    ba

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Route module for API
+Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
@@ -14,23 +14,26 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == 'auth':
+
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == 'basic_auth':
+elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ error handler for (unauthorized) 401 status code """
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ error handler for (forbidden) 403 status code """
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
@@ -43,16 +46,22 @@ def not_found(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ method to handler before request """
+    """ Before Request Handler
+    Requests Validation
+    """
     if auth is None:
         return
+
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
+
     if not auth.require_auth(request.path, excluded_paths):
         return
+
     if auth.authorization_header(request) is None:
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
 
